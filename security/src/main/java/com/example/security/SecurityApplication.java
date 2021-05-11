90ece55ef6d8eab048cb7e56faa19488476a3067
Gyanendro Kh <khgyanendro77@gmail.com>
2021-05-11 12:00:32 +0530

    Security Filter.

--- security/src/main/java/com/example/security/SecurityApplication.java
+++ security/src/main/java/com/example/security/SecurityApplication.java
@@ -1,13 +1,24 @@
 package com.example.security;
 
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @SpringBootApplication
 public class SecurityApplication {
@@ -25,9 +36,35 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
       .csrf().disable()
+      .addFilter(new TestFilter())
+      .addFilterAfter(new TestFilter2(), LogoutFilter.class)
       .authorizeRequests()
       .anyRequest().permitAll();
+  }
+}
+
+class TestFilter extends UsernamePasswordAuthenticationFilter {
+
+  @Override
+  public Authentication attemptAuthentication(
+    HttpServletRequest request, HttpServletResponse response
+  ) throws AuthenticationException {
+    return super.attemptAuthentication(request, response);
+  }
+}
+
+class TestFilter2 extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(
+    HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+  ) throws ServletException, IOException {
+    response.addHeader("X-TEST-2", "test");
+
+    filterChain.doFilter(request, response);
   }
 }
 
